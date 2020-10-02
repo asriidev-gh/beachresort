51ed27fb9f16a38ec9f552b44e087cc8ad864647
asriidev-gh <asriidev@gmail.com>
2020-10-02 19:26:37 +0800

    initial commit

---
+++ src/context.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+const RoomContext = React.createContext();
+
+export default class RoomProvider extends Component {
+  state = {};
+  render() {
+    return <div></div>;
+  }
+}
